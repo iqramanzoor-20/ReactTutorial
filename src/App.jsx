307c83eb3a0f2b0428e7b9ce5dfb821bcd224e06
iqramanzoor -20 <iqramanzoorcsb@gmail.com>
2026-08-06 03:26:25 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -13,46 +13,46 @@
 
 
 
-// import Chai from './Chai.jsx' 
-
-// function App() {
-//   return (   
-//     <div>
-//       <h1>Hello</h1>
-//       <Chai />  
-//     </div>
-//   )
-// }
-
-// export default App
-
-import { useState } from "react";
-import "./App.css";
+import Chai from './Chai.jsx' 
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="box" style={{padding: '20px', textAlign: 'center'}}>
-      <h1>Counter App</h1>
-      <h2>{count}</h2>
-
-      <div style={{marginTop: '10px'}}>
-        <button style={{margin: '5px'}} onClick={() => setCount(count + 1)}>
-          Increase
-        </button>
-
-        <button style={{margin: '5px'}} onClick={() => setCount(count - 1)}>
-          Decrease
-        </button>
-        
-        <button style={{margin: '5px'}} onClick={() => setCount(0)}>
-          Reset
-        </button>
-      </div>
-
+  return (   
+    <div>
+      <h1>Hello</h1>
+      <Chai />  
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+// import { useState } from "react";
+// import "./App.css";
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div className="box" style={{padding: '20px', textAlign: 'center'}}>
+//       <h1>Counter App</h1>
+//       <h2>{count}</h2>
+
+//       <div style={{marginTop: '10px'}}>
+//         <button style={{margin: '5px'}} onClick={() => setCount(count + 1)}>
+//           Increase
+//         </button>
+
+//         <button style={{margin: '5px'}} onClick={() => setCount(count - 1)}>
+//           Decrease
+//         </button>
+        
+//         <button style={{margin: '5px'}} onClick={() => setCount(0)}>
+//           Reset
+//         </button>
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// export default App;
