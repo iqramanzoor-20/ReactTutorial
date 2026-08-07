079d8ac365c60e2f214232c631344787757e4cfb
iqramanzoor -20 <iqramanzoorcsb@gmail.com>
2026-08-06 18:12:59 -0700

    counter app update

--- src/App.jsx
+++ src/App.jsx
@@ -14,33 +14,3 @@ function App() {
 }
 
 export default App
-// import { useState } from "react";
-// import "./App.css";
-
-// function App() {
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <div className="box" style={{padding: '20px', textAlign: 'center'}}>
-//       <h1>Counter App</h1>
-//       <h2>{count}</h2>
-
-//       <div style={{marginTop: '10px'}}>
-//         <button style={{margin: '5px'}} onClick={() => setCount(count + 1)}>
-//           Increase
-//         </button>
-
-//         <button style={{margin: '5px'}} onClick={() => setCount(count - 1)}>
-//           Decrease
-//         </button>
-        
-//         <button style={{margin: '5px'}} onClick={() => setCount(0)}>
-//           Reset
-//         </button>
-//       </div>
-
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -21,3 +21,12 @@ createRoot(document.getElementById('root')).render(
     </div>
   </StrictMode>
 )
+
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
