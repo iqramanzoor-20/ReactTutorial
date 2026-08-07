e1cd472bdd79270364abd433cb36fdab67e23caa
iqramanzoor -20 <iqramanzoorcsb@gmail.com>
2026-08-06 18:55:59 -0700

    ubdate all

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
 // import home from "./pages/Home"
-import Chai from './Chai.jsx' 
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import React from 'react'
+import './index.css'
+
+
+// import Chai from './Chai.jsx' 
+
+// function App() {
+//   const username = "chai aur code"
+
+//   return (
+//     <>
+//       <Chai/>
+//       <h1>chai aur react {username}</h1>
+//       <p>test para</p>
+//     </>
+//   )
+// }
+
+// export default App
+import { useState } from 'react'
 
 function App() {
-  const username = "chai aur code"
+
+  const [counter, setCounter] = useState(15)  // <-- ye change
+
+  const addValue = () => {
+    console.log("clicked", counter);
+    setCounter(counter + 1)  // <-- ye change
+  }
+
+  const removeValue = () => {
+    if(counter > 0){
+      setCounter(counter - 1)  // <-- ye change
+    }
+  }
 
   return (
     <>
-      <Chai/>
-      <h1>chai aur react {username}</h1>
-      <p>test para</p>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={removeValue}>remove value</button>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import React from 'react'
-import './index.css'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import React from 'react'
+// import './index.css'
 
-const reactElement = React.createElement(
-  'a',
-  {href: 'https://google.com', target: '_blank'},
-  'click me to visit google'
-)
 
-const anotherElement = (
-  <a href="https://google.com" target='_blank'>Visit google</a>
-)
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <div style={{display: 'flex', gap: '10px'}}> 
-      {reactElement}
-      {anotherElement}
-    </div>
-  </StrictMode>
-)
-
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
+// const reactElement = React.createElement(
+//   'a',
+//   {href: 'https://google.com', target: '_blank'},
+//   'click me to visit google'
 // )
+
+// const anotherElement = (
+//   <a href="https://google.com" target='_blank'>Visit google</a>
+// )
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <div style={{display: 'flex', gap: '10px'}}> 
+//       {reactElement}
+//       {anotherElement}
+//     </div>
+//   </StrictMode>
+// )
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client' 
+import App from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
